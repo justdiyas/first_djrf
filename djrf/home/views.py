c6b955a933f6d3b003027cda17aa69f646986a86
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-05-14 19:22:46 +0500

    Added api directory in a project root directory. Created index view to test get and post api. Mapped view to url

--- djrf/home/views.py
+++ djrf/home/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def index(request):
+    courses = {
+        'course_name': 'Python Backend Development',
+        'course_difficulty': 'Easy to Medium',
+        'course_subjects': ['Python Fundamentals', 'Django', ' Django RestFrameWork', 'SQL basics']
+    }
+
+    if request.method == 'GET':
+        print('You get the data via GET request')
+        return Response(courses)
+    elif request.method == 'POST':
+        print('You get the data  via POST request')
+        return Response(courses) 
