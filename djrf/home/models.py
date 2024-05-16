eb9d3bf2cd454e3f0772a20af9cd77ff08704b88
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-05-16 13:29:37 +0500

    Added Person model with attributes. Modified index view adding PUT, DELETE methods

--- djrf/home/models.py
+++ djrf/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    major = models.CharField(max_length=200)
+    city_born = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- djrf/home/views.py
+++ djrf/home/views.py
@@ -1,17 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def index(request):
     courses = {
         'course_name': 'Python Backend Development',
         'course_difficulty': 'Easy to Medium',
         'course_subjects': ['Python Fundamentals', 'Django', ' Django RestFrameWork', 'SQL basics']
     }
-
     if request.method == 'GET':
         print('You get the data via GET request')
         return Response(courses)
     elif request.method == 'POST':
-        print('You get the data  via POST request')
-        return Response(courses) 
+        data = request.data
+        print('You updated the data via POST request')
+        print(data)
+        return Response(courses)
+    elif request.method == 'PUT':
+        print('You modified the data via PUT request')
+        return Response(courses)
+    elif request.method == 'DELETE':
+        print('You deleted the data via DELETE request')
+        return Response(courses)
+
