b92fb801f8ba96188a1840559eb92901c770376a
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-05-17 11:30:27 +0500

    Added new Sport model and foreign key field to Person model

--- djrf/home/admin.py
+++ djrf/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Person, Sport
 
-# Register your models here.
+admin.site.register(Person)
+admin.site.register(Sport)
--- djrf/home/models.py
+++ djrf/home/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 
+
+class Sport(models.Model):
+    sport_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     major = models.CharField(max_length=200)
     city_born = models.CharField(max_length=200)
+    sport = models.ForeignKey(Sport, null=True, blank=True, related_name='favorite_sport', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
