c23b2541b8465b73f1def5f973380f08fdcd6dd6
Diyas Iyemberdiyev <iyemberdiyev@gmail.com>
2024-05-17 11:06:57 +0500

    Added validate method in PersonSerializer class

--- djrf/home/serializers.py
+++ djrf/home/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import Person
 
+
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
+
+    def validate(self, data):
+        special_characters = '~`!@#$%^&*()_-+={}[]|:;"<,>.?/'
+        if any(sym in special_characters for sym in data['name']):
+            raise serializers.ValidationError('Name field cannot contain a special character')
+        if any(sym in special_characters for sym in data['major']):
+            raise serializers.ValidationError('Major field cannot contain a special character')
+        if any(sym in special_characters for sym in data['city_born']):
+            raise serializers.ValidationError('City born field cannot contain a special character')
+        if data['age'] < 18:
+            raise serializers.ValidationError('Age must be greater than 18')
+        return data
